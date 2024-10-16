37c2bc88fd8ccdbe29cffcee90e482570a5e976e
Anuradha <anuradhaah667@gmail.com>
2024-10-16 10:21:59 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+ 
+  const[val,setVal] = useState("")
   return (
     <div>
         {/* Do not remove the main div */}
+        <input onChange={(e)=>{
+          setVal(e.target.value)
+        }} value={val}></input>
+        <h1>Hello {val}</h1>
     </div>
   )
 }
